e7102ede36bccba7b7c44891ad3be7e8d89ca358
Thomas <thomasjamesfowler97@gmail.com>
2021-02-03 17:00:44 +1100

    Created PdfRosterExtractor.kt to move all extraction logic out of Main.kt

--- roster-extractor/src/main/kotlin/com/tfowl/rosters/Main.kt
+++ roster-extractor/src/main/kotlin/com/tfowl/rosters/Main.kt
@@ -1,58 +1,19 @@
 package com.tfowl.rosters
 
-import com.jakewharton.picnic.Table
-import com.tfowl.rosters.detection.IntersectionDetector
-import com.tfowl.rosters.detection.TableExtractor
 import org.apache.pdfbox.pdmodel.PDDocument
-import org.apache.pdfbox.pdmodel.PDPage
 import java.io.File
-
-/*
-
-Grid Detection Algorithm:
-
-1) Extract all lines
-2) Identify intersection points
-3) Align intersection points along horizontals and verticals
-4) Define grid areas as rectangles bounded by 4 intersection points
-5) Define a cell as a grid area with some (row,col) position and (rowSpan,colSpan) extents
-6) Assign each text element into its grid area / cell reference
-7) Profit
-
-Things to consider:
-1) Multiple tables? [Not applicable in this domain]
-    Essentially boils down to finding sub-graphs of lines & intersections
-
- */
-
-private fun obtainTable(page: PDPage, debugger: VisualDebugger): Table {
-    val detection = IntersectionDetector()
-        .detect(
-            page,
-            detectionTolerance = 0.275,
-            combineTolerance = 0.275,
-            alignmentTolerance = 2.0,
-            debugger
-        )
-
-    return TableExtractor().extract(page, detection, debugger)
-}
 
 fun main(vararg args: String) {
     require(args.isNotEmpty()) { "Usage: [exec] roster-file" }
 
     val document = PDDocument.load(File(args[0]))
+    val extractor = PdfRosterExtractor()
+    val rosters = extractor.extract(document)
 
-    val tables = document.pages.map { page ->
-        val visualiser = NoOpVisualDebugger()
-        obtainTable(page, visualiser)
-    }
-    val rosters = tables.extractDepartmentRosters()
+    rosters.forEach { departmentRoster ->
+        println(departmentRoster.department)
 
-    rosters.forEach { department ->
-        println(department.department)
-
-        department.employees.forEach { employee ->
+        departmentRoster.employees.forEach { employee ->
             println("\t${employee.name}")
 
             employee.jobs.forEach { job ->

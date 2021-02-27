ee0dd4b04c9c214c4e1881fd7d599f15e3c6584c
Thomas <thomasjamesfowler97@gmail.com>
2021-02-27 20:48:21 +1100

    Add junit dependencies for future tests

--- roster-extractor/build.gradle.kts
+++ roster-extractor/build.gradle.kts
@@ -4,4 +4,10 @@ dependencies {
     implementation("org.apache.pdfbox:pdfbox:2.0.21")
     implementation("com.jakewharton.picnic:picnic:0.5.0")
     implementation("org.joml:joml:1.9.25")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
 }

8201f875ad1bc6509bd695afbd3d2c2f307a1664
Thomas <thomasjamesfowler97@gmail.com>
2021-03-18 18:05:14 +1100

    Added logging feature dependency and changed logging config to allow all logs (for dev)

--- workjam/build.gradle.kts
+++ workjam/build.gradle.kts
@@ -13,4 +13,5 @@ dependencies {
     implementation("io.ktor:ktor-client-core:1.5.2")
     implementation("io.ktor:ktor-client-cio:1.5.2")
     implementation("io.ktor:ktor-client-serialization-jvm:1.5.2")
+    implementation("io.ktor:ktor-client-logging:1.5.2")
 }

3828478064a1db7f07821629561ec2e681c16a29
Thomas <thomasjamesfowler97@gmail.com>
2021-03-18 17:21:23 +1100

    Add ktor dependencies

--- workjam/build.gradle.kts
+++ workjam/build.gradle.kts
@@ -9,4 +9,7 @@ dependencies {
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.6.0")
+
+    implementation("io.ktor:ktor-client-core:1.5.2")
+    implementation("io.ktor:ktor-client-serialization-jvm:1.5.2")
 }

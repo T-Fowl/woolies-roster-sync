ed5574f2dbb1bed5980b368bd9d198c59d3b883b
Thomas <thomasjamesfowler97@gmail.com>
2021-03-23 16:50:09 +1100

    Added DataStorage class wrapping a DataStore with the StringFormat & KSerialiser bundled together

--- google-api-tfowl/src/main/kotlin/com/tfowl/googleapi/DataStoreExt.kt
+++ google-api-tfowl/src/main/kotlin/com/tfowl/googleapi/DataStoreExt.kt
@@ -3,23 +3,46 @@
 package com.tfowl.googleapi
 
 import com.google.api.client.util.store.DataStore
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.StringFormat
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.encodeToString
+import kotlinx.serialization.*
 
+@Deprecated("Use DataStorage instead")
 inline fun <reified V> DataStore<String>.get(format: StringFormat, key: String): V? =
     get(key)?.let { format.decodeFromString(it) }
 
+@Deprecated("Use DataStorage instead")
 inline fun <reified V> DataStore<String>.set(
     format: StringFormat,
     key: String,
     value: V,
 ): DataStore<String> = set(key, format.encodeToString(value))
 
+@Deprecated("Use DataStorage instead")
 @Suppress("RedundantSuspendModifier")
 suspend inline fun <reified V> DataStore<String>.computeIfAbsent(
     format: StringFormat,
     key: String,
     provider: (String) -> V,
 ): V = get(format, key) ?: provider(key).also { set(format, key, it) }
+
+class DataStorage<V>(
+    private val store: DataStore<String>,
+    private val format: StringFormat,
+    private val serializer: KSerializer<V>,
+) {
+    fun get(key: String): V? = store.get(key)?.let { format.decodeFromString(serializer, it) }
+
+    fun set(key: String, value: V): DataStorage<V> {
+        store.set(key, format.encodeToString(serializer, value))
+        return this
+    }
+}
+
+suspend fun <V> DataStorage<V>.computeIfAbsent(key: String, provider: suspend (String) -> V): V {
+    return get(key) ?: provider(key).also { set(key, it) }
+}
+
+inline fun <reified V> DataStore<String>.asDataStorage(format: StringFormat): DataStorage<V> =
+    asDataStorage(format, serializer())
+
+fun <V> DataStore<String>.asDataStorage(format: StringFormat, serialiser: KSerializer<V>): DataStorage<V> =
+    DataStorage(this, format, serialiser)

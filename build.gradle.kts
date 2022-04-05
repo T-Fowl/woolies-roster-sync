a45289860b078636b039ae1ecbabe6d251e64172
Thomas <thomasjamesfowler97@gmail.com>
2022-04-05 13:13:05 +1000

    Updated all dependencies and kotlin gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.5.31"
-    kotlin("plugin.serialization") version "1.5.31" apply false
+    kotlin("jvm") version "1.6.20"
+    kotlin("plugin.serialization") version "1.6.20" apply false
     application
 }
 
@@ -31,26 +31,26 @@ allprojects {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
 
-    implementation("com.github.ajalt.clikt:clikt:3.2.0")
+    implementation("com.github.ajalt.clikt:clikt:3.4.0")
 
-    implementation("com.google.api-client:google-api-client:1.32.1")
-    implementation("com.google.oauth-client:google-oauth-client-jetty:1.32.1")
-    implementation("com.google.apis:google-api-services-calendar:v3-rev20210804-1.32.1")
+    implementation("com.google.api-client:google-api-client:1.33.4")
+    implementation("com.google.oauth-client:google-oauth-client-jetty:1.33.1")
+    implementation("com.google.apis:google-api-services-calendar:v3-rev20220203-1.32.1")
 
-    implementation("com.michael-bull.kotlin-result:kotlin-result:1.1.13")
-    implementation("com.michael-bull.kotlin-result:kotlin-result-coroutines:1.1.13")
+    implementation("com.michael-bull.kotlin-result:kotlin-result:1.1.14")
+    implementation("com.michael-bull.kotlin-result:kotlin-result-coroutines:1.1.14")
 
     implementation(project(":workjam"))
 
     implementation("com.github.spullara.mustache.java:compiler:0.9.10")
 
-    implementation("org.apache.logging.log4j:log4j-slf4j18-impl:2.14.1")
+    implementation("org.apache.logging.log4j:log4j-slf4j18-impl:2.17.1")
 
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.0")
-    testImplementation("io.mockk:mockk:1.12.0")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("io.mockk:mockk:1.12.3")
 }
 
 tasks.withType<Test> {
--- workjam/build.gradle.kts
+++ workjam/build.gradle.kts
@@ -4,10 +4,10 @@ plugins {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
 
-    implementation("io.ktor:ktor-client-core:1.6.3")
-    implementation("io.ktor:ktor-client-cio:1.6.3")
-    implementation("io.ktor:ktor-client-serialization:1.6.3")
-    implementation("io.ktor:ktor-client-logging:1.6.3")
+    implementation("io.ktor:ktor-client-core:1.6.8")
+    implementation("io.ktor:ktor-client-cio:1.6.8")
+    implementation("io.ktor:ktor-client-serialization:1.6.8")
+    implementation("io.ktor:ktor-client-logging:1.6.8")
 }

a23bdbadc27d82cd78dfe650684f7f0017f9211c
Thomas <thomasjamesfowler97@gmail.com>
2022-04-13 17:48:38 +1000

    Added criteria for `PM Trucks` shifts with an early start

--- src/main/kotlin/com/tfowl/woolies/sync/transform/SummaryGenerator.kt
+++ src/main/kotlin/com/tfowl/woolies/sync/transform/SummaryGenerator.kt
@@ -13,14 +13,23 @@ interface SummaryGenerator {
 }
 
 object DefaultSummaryGenerator : SummaryGenerator {
+
+    // TODO: There should probably be a way to specify this in a configuration file
     override fun generate(shift: Shift): String {
         val start = LocalTime.ofInstant(shift.event.startDateTime, shift.event.location.timeZoneID)
-//        val end = LocalTime.ofInstant(shift.event.endDateTime, shift.event.location.timeZoneID)
+        val end = LocalTime.ofInstant(shift.event.endDateTime, shift.event.location.timeZoneID)
 
         /* Trucks */
         when (start) {
             LocalTime.of(4, 30), LocalTime.of(5, 30) -> return "AM Trucks"
             LocalTime.of(13, 0)                      -> return "PM Trucks"
+        }
+
+        /* For me this is likely to mean an early-start for a PM Trucks shift */
+        if(end == LocalTime.of(16, 0) || end == LocalTime.of(18, 0)) {
+            when(start.hour) {
+                in 0 .. 13 -> return "Picking AM and PM Trucks"
+            }
         }
 
         /* General Picking */

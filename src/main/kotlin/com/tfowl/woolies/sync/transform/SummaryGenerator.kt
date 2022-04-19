13ea8004d45012304601d972bae9cbb411175f89
Thomas <thomasjamesfowler97@gmail.com>
2022-04-19 19:35:04 +1000

    Improved SummaryGenerator to look at shift segments now that they are being used for online truck shifts

--- src/main/kotlin/com/tfowl/woolies/sync/transform/SummaryGenerator.kt
+++ src/main/kotlin/com/tfowl/woolies/sync/transform/SummaryGenerator.kt
@@ -1,5 +1,6 @@
 package com.tfowl.woolies.sync.transform
 
+import com.tfowl.workjam.client.model.Segment
 import com.tfowl.workjam.client.model.Shift
 import java.time.LocalTime
 
@@ -12,33 +13,65 @@ interface SummaryGenerator {
     fun generate(shift: Shift): String
 }
 
+private val Shift.startTime: LocalTime get() = LocalTime.ofInstant(event.startDateTime, event.location.timeZoneID)
+private val Shift.endTime: LocalTime get() = LocalTime.ofInstant(event.endDateTime, event.location.timeZoneID)
+private val Segment.startTime: LocalTime get() = LocalTime.ofInstant(startDateTime, location.timeZoneID)
+
+data class ShiftRundown(
+    val amTrucks: Boolean = false,
+    val pmTrucks: Boolean = false,
+    val amStart: Boolean = false,
+    val pmStart: Boolean = false,
+    val amFinish: Boolean = false,
+    val pmFinish: Boolean = false
+)
+
 object DefaultSummaryGenerator : SummaryGenerator {
 
     // TODO: There should probably be a way to specify this in a configuration file
     override fun generate(shift: Shift): String {
-        val start = LocalTime.ofInstant(shift.event.startDateTime, shift.event.location.timeZoneID)
-        val end = LocalTime.ofInstant(shift.event.endDateTime, shift.event.location.timeZoneID)
 
-        /* Trucks */
-        when (start) {
-            LocalTime.of(4, 30), LocalTime.of(5, 30) -> return "AM Trucks"
-            LocalTime.of(13, 0)                      -> return "PM Trucks"
+        var rundown = ShiftRundown(
+            amStart = shift.startTime < LocalTime.NOON,
+            pmStart = shift.startTime >= LocalTime.NOON,
+            amFinish = shift.endTime < LocalTime.NOON,
+            pmFinish = shift.endTime >= LocalTime.NOON
+        )
+
+        val segmentGroups = shift.segments.groupBy { it.position.name.lowercase() }
+
+        /* If the shift is set up correctly with different segments, including dispatch: */
+        if ("online dispatch" in segmentGroups) {
+            val dispatchSegments = segmentGroups["online dispatch"]!!
+
+            if (dispatchSegments.any { segment -> segment.startTime < LocalTime.NOON })
+                rundown = rundown.copy(amTrucks = true)
+            if (dispatchSegments.any { segment -> segment.startTime >= LocalTime.NOON })
+                rundown = rundown.copy(pmTrucks = true)
         }
 
-        /* For me this is likely to mean an early-start for a PM Trucks shift */
-        if(end == LocalTime.of(16, 0) || end == LocalTime.of(18, 0)) {
-            when(start.hour) {
-                in 0 .. 13 -> return "Picking AM and PM Trucks"
-            }
-        }
+        /* If the shift isn't set up correctly with dispatch segments: */
 
-        /* General Picking */
-        when (start.hour) {
-            in 0..13  -> return "Picking AM"
-            in 16..23 -> return "Picking PM"
-        }
+        // AM Trucks are usually 4:30 or 5:30, this is for historical shifts where I started earlier
+        if (shift.startTime <= LocalTime.of(5, 30))
+            rundown = rundown.copy(amTrucks = true)
 
-        /* idk */
-        return shift.event.title?.removePrefix("*SUP-")?.removePrefix("SUP-") ?: "Error: Missing Title"
+        // Doesn't include starting early, but if I'm already at work doing PM trucks doesn't really matter
+        if (shift.startTime == LocalTime.of(13, 0))
+            rundown = rundown.copy(pmTrucks = true)
+
+
+        return when {
+            rundown.amTrucks && rundown.pmTrucks -> "AM & PM Trucks"
+            rundown.amTrucks                     -> "AM Trucks"
+            rundown.amStart && rundown.pmTrucks  -> "AM + PM Trucks"
+            rundown.pmTrucks                     -> "PM Trucks"
+            // TODO: Should these be changed to just "AM" & "PM" as I am not guaranteed to be only picking
+            rundown.amStart                      -> "Picking AM"
+            rundown.pmStart                      -> "Picking PM"
+            /* idk */
+            else                                 -> shift.event.title?.removePrefix("*SUP-")?.removePrefix("SUP-")
+                ?: "Error: Missing Title"
+        }
     }
 }

8133975c36ebbc8f1f908ec2560c0412117d3430
Thomas <thomasjamesfowler97@gmail.com>
2022-05-02 15:02:54 +1000

    Changed the root command name to `woolies-roster`

--- src/main/kotlin/com/tfowl/woolies/sync/Main.kt
+++ src/main/kotlin/com/tfowl/woolies/sync/Main.kt
@@ -7,7 +7,7 @@ import com.github.ajalt.clikt.output.CliktHelpFormatter
 import com.tfowl.woolies.sync.commands.Authorise
 import com.tfowl.woolies.sync.commands.Sync
 
-class WooliesRosterSync : NoOpCliktCommand() {
+class WooliesRosterCommand : NoOpCliktCommand(name = "woolies-roster") {
     init {
         context {
             helpFormatter = CliktHelpFormatter(
@@ -18,6 +18,6 @@ class WooliesRosterSync : NoOpCliktCommand() {
     }
 }
 
-fun main(vararg args: String) = WooliesRosterSync().subcommands(
+fun main(vararg args: String) = WooliesRosterCommand().subcommands(
     Sync(), Authorise()
 ).main(args)

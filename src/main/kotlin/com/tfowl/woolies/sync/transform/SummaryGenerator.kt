e85c47b112650aca1e835404142dc0a510f80e00
Thomas <thomasjamesfowler97@gmail.com>
2022-06-30 18:37:19 +1000

    Changed the behaviour of SummaryGenerator - simplified truck shifts, removed "Picking" prefix and added support for supervisor

--- src/main/kotlin/com/tfowl/woolies/sync/transform/SummaryGenerator.kt
+++ src/main/kotlin/com/tfowl/woolies/sync/transform/SummaryGenerator.kt
@@ -1,8 +1,6 @@
 package com.tfowl.woolies.sync.transform
 
-import com.tfowl.workjam.client.model.Segment
 import com.tfowl.workjam.client.model.Shift
-import com.tfowl.workjam.client.model.endTime
 import com.tfowl.workjam.client.model.startTime
 import java.time.LocalTime
 
@@ -17,60 +15,41 @@ interface SummaryGenerator {
     fun generate(shift: Shift): String
 }
 
-data class ShiftRundown(
-    val amTrucks: Boolean = false,
-    val pmTrucks: Boolean = false,
-    val amStart: Boolean = false,
-    val pmStart: Boolean = false,
-    val amFinish: Boolean = false,
-    val pmFinish: Boolean = false
-)
-
 object DefaultSummaryGenerator : SummaryGenerator {
 
     // TODO: There should probably be a way to specify this in a configuration file
     override fun generate(shift: Shift): String {
-
-        var rundown = ShiftRundown(
-            amStart = shift.startTime < LocalTime.NOON,
-            pmStart = shift.startTime >= LocalTime.NOON,
-            amFinish = shift.endTime < LocalTime.NOON,
-            pmFinish = shift.endTime >= LocalTime.NOON
-        )
-
-        val segmentGroups = shift.segments.groupBy { it.position.name.lowercase() }
-
         /* If the shift is set up correctly with different segments, including dispatch: */
+        val segmentGroups = shift.segments.groupBy { it.position.name.removeSupPrefix().lowercase() }
+
+        if (segmentGroups.keys.any { "supervisor" in it }) {
+            return "Supervisor"
+        }
+
         if ("online dispatch" in segmentGroups) {
             val dispatchSegments = segmentGroups["online dispatch"]!!
 
             if (dispatchSegments.any { segment -> segment.startTime < LocalTime.NOON })
-                rundown = rundown.copy(amTrucks = true)
+                return "AM Trucks"
             if (dispatchSegments.any { segment -> segment.startTime >= LocalTime.NOON })
-                rundown = rundown.copy(pmTrucks = true)
+                return "PM Trucks"
+            return "Trucks"
         }
 
         /* If the shift isn't set up correctly with dispatch segments: */
 
         // AM Trucks are usually 4:30 or 5:30, this is for historical shifts where I started earlier
         if (shift.startTime <= LocalTime.of(5, 30))
-            rundown = rundown.copy(amTrucks = true)
+            return "AM Trucks"
 
         // Doesn't include starting early, but if I'm already at work doing PM trucks doesn't really matter
         if (shift.startTime == LocalTime.of(13, 0))
-            rundown = rundown.copy(pmTrucks = true)
-
+            return "PM Trucks"
 
         return when {
-            rundown.amTrucks && rundown.pmTrucks -> "AM & PM Trucks"
-            rundown.amTrucks                     -> "AM Trucks"
-            rundown.amStart && rundown.pmTrucks  -> "AM + PM Trucks"
-            rundown.pmTrucks                     -> "PM Trucks"
-            // TODO: Should these be changed to just "AM" & "PM" as I am not guaranteed to be only picking
-            rundown.amStart                      -> "Picking AM"
-            rundown.pmStart                      -> "Picking PM"
-            /* idk */
-            else                                 -> shift.event.title?.removeSupPrefix() ?: "Error: Missing Title"
+            shift.startTime < LocalTime.NOON  -> "AM"
+            shift.startTime >= LocalTime.NOON -> "PM"
+            else                              -> shift.event.title?.removeSupPrefix() ?: "Error: Missing Title"
         }
     }
 }

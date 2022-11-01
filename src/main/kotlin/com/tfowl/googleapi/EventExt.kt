067314d51b4b172fb68588b44fbeab1f775a8030
Thomas <thomasjamesfowler97@gmail.com>
2022-11-01 20:31:32 +1100

    Added utility functions for setting event extended properties with client checks for property lengths and sizes

--- src/main/kotlin/com/tfowl/googleapi/EventExt.kt
+++ src/main/kotlin/com/tfowl/googleapi/EventExt.kt
@@ -16,3 +16,25 @@ fun Event.setStart(instant: Instant, zone: ZoneId = ZoneId.systemDefault()): Eve
 
 fun Event.setEnd(instant: Instant, zone: ZoneId = ZoneId.systemDefault()): Event =
     setEnd(instant.toGoogleEventDateTime(zone))
+
+fun Event.ExtendedProperties.setSharedChecked(properties: Map<String, String>): Event.ExtendedProperties =
+    setShared(properties.checkExtendedProperties())
+
+fun Event.ExtendedProperties.setPrivateChecked(properties: Map<String, String>): Event.ExtendedProperties =
+    setPrivate(properties.checkExtendedProperties())
+
+private fun Map<String, String>.checkExtendedProperties(): Map<String, String> {
+    check(size <= 300) { "$size is more than the 300 extended properties count limit" }
+
+    var sum = 0
+    for ((key, value) in this) {
+        sum += key.length + value.length
+        check(key.length <= 44) { "Property key is longer than 44 characters and will be silently dropped: $key" }
+        check(value.length <= 1024) { "Property is longer than 1024 characters and will be silently truncated: $value" }
+    }
+
+    check(sum <= 32000) { "${sum / 1024}kB is larger than the 32kB size limit for extended properties" }
+
+
+    return this
+}

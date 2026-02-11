12154d2630fd1c6ed4293182a3feb4a2b5f653b6
Thomas <thomasjamesfowler97@gmail.com>
2026-02-11 17:49:23 +1100

    Moved localdate option conversion into its own file. Forgot to commit earlier

---
+++ src/main/kotlin/com/tfowl/woolies/sync/commands/options/conversions.kt
@@ -0,0 +1,11 @@
+package com.tfowl.woolies.sync.commands.options
+
+import com.github.ajalt.clikt.parameters.options.NullableOption
+import com.github.ajalt.clikt.parameters.options.RawOption
+import com.github.ajalt.clikt.parameters.options.convert
+import com.tfowl.woolies.sync.utils.toLocalDateOrNull
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+fun RawOption.localDate(formatter: DateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE): NullableOption<LocalDate, LocalDate> =
+    convert("LOCAL_DATE") { it.toLocalDateOrNull(formatter) ?: fail("A date in the $formatter format is required") }

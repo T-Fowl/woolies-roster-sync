37218d2b6cfd15adc56b42a6f3ed9fc774200729
Thomas <thomasjamesfowler97@gmail.com>
2026-02-14 18:00:08 +1100

    Added console prompts for email, password and token

--- src/main/kotlin/com/tfowl/woolies/sync/commands/options/auth.kt
+++ src/main/kotlin/com/tfowl/woolies/sync/commands/options/auth.kt
@@ -2,9 +2,7 @@ package com.tfowl.woolies.sync.commands.options
 
 import com.auth0.jwt.JWT
 import com.github.ajalt.clikt.parameters.groups.OptionGroup
-import com.github.ajalt.clikt.parameters.options.convert
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.options.*
 import com.github.michaelbull.result.Ok
 import com.github.michaelbull.result.Result
 import com.github.michaelbull.result.binding
@@ -15,24 +13,28 @@ import com.tfowl.woolies.sync.findTokenCookie
 import com.tfowl.woolies.sync.login
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.slf4j.spi.LoggingEventBuilder
+import java.time.Instant
 
 private val LOGGER = LoggerFactory.getLogger("com.tfowl.woolies.sync.commands.options.auth")
 
 sealed class AuthMethod(name: String) : OptionGroup(name)
 
 class BrowserAuthentication : AuthMethod("Options for browser authentication") {
-    val email by option("--email", envvar = "WORKJAM_EMAIL").required()
-    val password by option("--password", envvar = "WORKJAM_PASSWORD").required()
+    val email by option("--email", envvar = "WORKJAM_EMAIL")
+        .prompt("Enter your email address")
+
+    val password by option("--password", envvar = "WORKJAM_PASSWORD")
+        .prompt("Enter your password", hideInput = true)
+
     val playwrightUrl by option("--playwright-driver-url", envvar = "PLAYWRIGHT_DRIVER_URL").required()
 }
 
 class TokenAuthentication : AuthMethod("Options for token authentication") {
     val token by option("--token", envvar = "WORKJAM_TOKEN")
         .convert { JWT.decode(it) }
-        .required()
+        .prompt("Enter your token")
+        .check("Token must be current") { Instant.now() in (it.notBeforeAsInstant..it.expiresAtAsInstant) }
 }
 
 suspend fun AuthMethod.token(): Result<String, Any> = when (this) {

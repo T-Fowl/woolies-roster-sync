cc1674d5eb9be62c5a055667854316617bebb746
Thomas <thomasjamesfowler97@gmail.com>
2026-03-11 14:24:47 +1100

    Heavily simplified workjam client creation. Token management is now the responsibility of the user

--- workjam/src/main/kotlin/com/tfowl/workjam/client/WorkjamClient.kt
+++ workjam/src/main/kotlin/com/tfowl/workjam/client/WorkjamClient.kt
@@ -1,14 +1,28 @@
 package com.tfowl.workjam.client
 
 import com.tfowl.workjam.client.model.*
+import com.tfowl.workjam.client.model.serialisers.InstantSerialiser
 import io.ktor.client.*
 import io.ktor.client.call.*
+import io.ktor.client.engine.*
+import io.ktor.client.engine.java.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.logging.*
 import io.ktor.client.request.*
 import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.contextual
 import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 interface WorkjamClient {
-    val userId: String
+    val user: WorkjamUser
+
+    val userId: String get() = user.userId.toString()
 
     suspend fun employees(company: String): List<Employee>
 
@@ -38,25 +52,21 @@ interface WorkjamClient {
 
     suspend fun availability(company: String, employee: String, event: String): Availability
 
-    suspend fun shifts(company: String, location: String, startDateTime: OffsetDateTime, endDateTime: OffsetDateTime, includeOverlaps: Boolean = true): List<Shift>
+    suspend fun shifts(
+        company: String,
+        location: String,
+        startDateTime: OffsetDateTime,
+        endDateTime: OffsetDateTime,
+        includeOverlaps: Boolean = true,
+    ): List<Shift>
 
     suspend fun periodicTimecards(company: String, employee: String): List<PeriodicTimecard>
 }
 
-class DefaultWorkjamClient internal constructor(
-    private val user: WorkjamUser,
+class KtorWorkjamClient internal constructor(
+    override val user: WorkjamUser,
     private val client: HttpClient,
-    private val defaultUrl: Url,
 ) : WorkjamClient {
-    override val userId: String = user.userId.toString()
-
-    private suspend inline fun <reified T> get(requestUrlBuilder: URLBuilder.() -> Unit): T {
-        return client.get {
-            header(WorkjamTokenHeader, user.token)
-            url.takeFrom(defaultUrl)
-            url.requestUrlBuilder()
-        }.body()
-    }
 
     override suspend fun employees(company: String): List<Employee> = get {
         path("api/v4/companies/$company/employees")
@@ -113,7 +123,7 @@ class DefaultWorkjamClient internal constructor(
         location: String,
         startDateTime: OffsetDateTime,
         endDateTime: OffsetDateTime,
-        includeOverlaps: Boolean
+        includeOverlaps: Boolean,
     ): List<Shift> = get {
         path("api/v4/companies/$company/locations/$location/shifts")
         parameters.append("startDateTime", startDateTime.toString())
@@ -125,8 +135,62 @@ class DefaultWorkjamClient internal constructor(
     override suspend fun periodicTimecards(company: String, employee: String): List<PeriodicTimecard> = get {
         path("api/v4/companies/$company/employees/$employee/periodic_timecards")
     }
-}
 
-class DevelopmentWorkjamClient(private val delegate: WorkjamClient) : WorkjamClient by delegate {
-    
+    private suspend inline fun <reified T> get(requestUrlBuilder: URLBuilder.() -> Unit): T {
+        return client.get {
+            header(HttpHeaders.Authorization, "Bearer ${user.token}")
+//            header("x-token", user.token)
+            url.requestUrlBuilder()
+        }.body()
+    }
+
+    companion object {
+        suspend fun create(
+            token: String,
+            engine: HttpClientEngine = Java.create(),
+            clientConfig: HttpClientConfig<*>.() -> Unit = {},
+        ): KtorWorkjamClient {
+            val client = HttpClient(engine) {
+                install(HttpTimeout) {
+                    requestTimeoutMillis = 30_000
+                }
+                install(DefaultRequest) {
+                    url("https://prod-aus-gcp-woolworths-api.workjam.com")
+                    header(HttpHeaders.AcceptLanguage, Locale.ENGLISH)
+                    header(HttpHeaders.Origin, "https://app.workjam.com")
+                    header(HttpHeaders.Referrer, "https://app.workjam.com/")
+                }
+                install(HttpRequestRetry) {
+                    retryOnServerErrors(maxRetries = 5)
+                    exponentialDelay()
+                }
+                install(ContentNegotiation) {
+                    json(Json {
+                        ignoreUnknownKeys = true
+                        serializersModule = SerializersModule {
+                            contextual(InstantSerialiser(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS[XX][XXX][ZZZ][OOOO]")))
+                        }
+                    })
+                }
+                install(Logging) {
+                    level = LogLevel.INFO
+                    logger = Logger.DEFAULT
+                }
+                // Hehe sneaky
+                install(UserAgent) {
+                    agent =
+                        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36"
+                }
+
+                this.apply(clientConfig)
+            }
+
+            val user = client.patch("/auth/v3") {
+                header(HttpHeaders.Authorization, "Bearer $token")
+//                header("x-token", token)
+            }.body<WorkjamUser>()
+
+            return KtorWorkjamClient(user, client)
+        }
+    }
 }
